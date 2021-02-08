70ee7660a1e92a41c4f7a22bc8c187c723edbe90
Roberto Santalla <roobre@roobre.es>
2021-02-08 16:15:38 +0100

    ci: parity with other release pipelines

--- build/release.mk
+++ build/release.mk
@@ -1,16 +1,16 @@
 BUILD_DIR    := ./bin/
-GORELEASER_VERSION := v0.143.0
+GORELEASER_VERSION := v0.154.0
 GORELEASER_BIN ?= bin/goreleaser
 
 bin:
 	@mkdir -p $(BUILD_DIR)
 
 $(GORELEASER_BIN): bin
-	@echo "=== $(INTEGRATION) === [$(GORELEASER_BIN)] Installing goreleaser $(GORELEASER_VERSION)"
+	@echo "===> $(INTEGRATION) === [$(GORELEASER_BIN)] Installing goreleaser $(GORELEASER_VERSION)"
 	@(wget -qO /tmp/goreleaser.tar.gz https://github.com/goreleaser/goreleaser/releases/download/$(GORELEASER_VERSION)/goreleaser_$(OS_DOWNLOAD)_x86_64.tar.gz)
 	@(tar -xf  /tmp/goreleaser.tar.gz -C bin/)
 	@(rm -f /tmp/goreleaser.tar.gz)
-	@echo "=== $(INTEGRATION) === [$(GORELEASER_BIN)] goreleaser downloaded"
+	@echo "===> $(INTEGRATION) === [$(GORELEASER_BIN)] goreleaser downloaded"
 
 .PHONY : release/clean
 release/clean:
